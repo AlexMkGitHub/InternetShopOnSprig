071897ae2a1c99b25ce9018e8376e2cd17bed273
alexm <mkalex77@gmail.com>
2022-05-31 23:09:18 +0700

    Практическое задание №3. С помощью AOP замерить время работы сервисов.

--- src/main/java/com/geekbrains/geekmarketwinter/utils/AppLoggingAspect.java
+++ src/main/java/com/geekbrains/geekmarketwinter/utils/AppLoggingAspect.java
@@ -1,14 +1,17 @@
 package com.geekbrains.geekmarketwinter.utils;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
 
 @Aspect
 @Component
 public class AppLoggingAspect {
-    @After("execution(public void com.geekbrains.geekmarketwinter.services.ShoppingCartService.addToCart(..))") // pointcut expression
+    @After("execution(public void com.geekbrains.geekmarketwinter.services.ShoppingCartService.addToCart(..))")
+    // pointcut expression
     public void aopSimpleMethod() {
         System.out.println("добален товар в корзину");
     }
@@ -37,5 +40,17 @@ public class AppLoggingAspect {
         }
     }
 
+    //С помощью AOP замерить время работы сервисов
+    @Around("execution(public * com.geekbrains.geekmarketwinter.services.*.*(..))")
+    public Object methodsCallTime(ProceedingJoinPoint callMethod) throws Throwable {
+        StopWatch workingTime = new StopWatch(callMethod.toString());
+        try {
+            workingTime.start(callMethod.toShortString());
+            return callMethod.proceed();
+        } finally {
+            workingTime.stop();
+            System.out.println(callMethod.getSignature().toShortString() + ": " + workingTime.getTotalTimeMillis() + " ms");
+        }
+    }
 
 }
