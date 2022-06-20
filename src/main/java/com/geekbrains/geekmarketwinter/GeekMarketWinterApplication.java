e8b7ae3846e59cf581d8ba27c11f8d0c3afa78ff
alexm <mkalex77@gmail.com>
2022-06-20 10:55:30 +0300

    Коррекция

--- src/main/java/com/geekbrains/geekmarketwinter/GeekMarketWinterApplication.java
+++ src/main/java/com/geekbrains/geekmarketwinter/GeekMarketWinterApplication.java
@@ -3,13 +3,13 @@ package com.geekbrains.geekmarketwinter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableEurekaServer
 @ComponentScan
-public class GeekMarketWinterApplication {
+@EnableAutoConfiguration
+public class
+GeekMarketWinterApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GeekMarketWinterApplication.class, args);
 	}
